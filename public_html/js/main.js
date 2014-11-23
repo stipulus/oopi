141c4cbbf5d8cf3526aae593fb0e780260fc14a1
Tyler Chase-Nason <tyler@stipulus.com>
2014-11-23 13:44:05 -0700

    Added output to main file

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -7,3 +7,17 @@ function init() {
     
     var vehicle = new Vehicle();
 }
+/*
+ * OUTPUT
+ * 
+1                                                           Library.js (line 8)
+2                                                           Library.js (line 9)
+3                                                           Library.js (line 10)
+say: hello                                                  Library.js (line 14)
+4                                                           Library.js (line 8)
+5                                                           Library.js (line 9)
+6                                                           Library.js (line 10)
+blue :(                                                     Library.js (line 23)
+Error: Cannot construct abstract class.
+    throw new Error('Cannot construct abstract class.');    oopi.js (line 72)
+ */
